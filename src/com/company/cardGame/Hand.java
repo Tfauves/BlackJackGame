6e4e886d768f56253d56c47dff249aa0ea400a99
Tfauves <tfauvelle@gmail.com>
2021-09-18 15:16:56 -0400

    ready to define methods.

---
+++ src/com/company/cardGame/Hand.java
@@ -0,0 +1,30 @@
+package com.company.cardGame;
+
+import com.company.actors.Player;
+
+public class Hand {
+    private Deck deck;
+    private Player player;
+    private int bet;
+
+
+
+    public void hit() {
+
+    }
+
+    public void stand() {
+
+    }
+
+    public void doubleDown() {
+
+    }
+
+    public void split() {
+
+    }
+
+//    public void getValue();
+
+}
