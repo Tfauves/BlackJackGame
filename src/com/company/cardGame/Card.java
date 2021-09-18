3372102c524b7ad34e5213672be6e250324f47b6
Tfauves <tfauvelle@gmail.com>
2021-09-17 21:01:19 -0400

    Card abstract class defined.

---
+++ src/com/company/cardGame/Card.java
@@ -0,0 +1,17 @@
+package com.company.cardGame;
+
+public abstract class Card {
+    private final String suit;
+    private final int faceValue;
+
+    public Card(String suit, int faceValue) {
+        this.suit = suit;
+        this.faceValue = faceValue;
+    }
+
+    public String toString() {
+        return faceValue + " " + suit;
+    }
+
+    public abstract void display();
+}
