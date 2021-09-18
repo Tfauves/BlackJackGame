5a5b080c05cfa750b1d2f852201aa8bb464d3c11
Tfauves <tfauvelle@gmail.com>
2021-09-18 14:39:22 -0400

    Dealer decalred, methods need defining.

---
+++ src/com/company/actors/Dealer.java
@@ -0,0 +1,13 @@
+package com.company.actors;
+
+public class Dealer implements Actor{
+    private final String NAME = "dealer";
+    private int balance = 0;
+    private final int BET = 0;
+
+
+    public void getName() {}
+    public void getBalance() {}
+    public void getBet() {}
+    public void getAction() {}
+}
