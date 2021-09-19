1e9e14cda11c6b0b2fbf3036979f34f52a091e92
Tfauves <tfauvelle@gmail.com>
2021-09-19 19:15:04 -0400

    declared and fields and methods in table class.

--- src/com/company/cardGame/Table.java
+++ src/com/company/cardGame/Table.java
@@ -1,4 +1,24 @@
 package com.company.cardGame;
 
+import com.company.actors.Dealer;
+
+import java.util.List;
+
 public class Table {
+    List hands;
+    List actors;
+    Dealer dealer;
+
+
+    public void displayBets() {
+
+    }
+
+    public void displayTable() {
+
+    }
+
+    public void removePlayer() {
+
+    }
 }
