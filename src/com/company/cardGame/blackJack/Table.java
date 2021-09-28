49f76b6b9d77115f52bd0cf500d90eafc73a3c0d
Travis J <tfauvelle@gmail.com>
2021-09-28 17:45:44 -0400

    work on hit, split, double.

--- src/com/company/cardGame/blackJack/Table.java
+++ src/com/company/cardGame/blackJack/Table.java
@@ -1,53 +1,56 @@
 package com.company.cardGame.blackJack;
-
 import com.company.cardGame.actors.Dealer;
 import com.company.cardGame.actors.Player;
 import com.company.cardGame.deck.Deck;
 import com.company.cardGame.deck.StandardDeck;
 
-import java.util.List;
-
 public class Table {
-     Hand player = new Hand(new Player("player"));
-     Hand dealer = new Hand(new Dealer());
-     Deck deck;
+    Hand player = new Hand(new Player("player"));
+    Hand dealer = new Hand(new Dealer());
+    Deck deck;
 
-     public void playRound() {
-         deck = new StandardDeck();
-         deck.shuffle();
-         deal();
-//         displayTable();
+    public void playRound() {
+        deck = new StandardDeck();
+        deck.shuffle();
+        /*
+        0. take bets
+        1. deal cards
+        b2. see who won
+        a2. players turn
+        a3. dealers turn
+        a4. see who one
+         */
+        deal();
+        displayTable();
         turn(player);
         turn(dealer);
-         determineWinner();
-
-     }
-
-     private void deal() {
-         for (int count = 0; count < 2; count++) {
-             player.addCard(deck.draw());
-             dealer.addCard(deck.draw());
-         }
-     }
+        determineWinner();
+    }
 
     private void displayTable() {
-         StringBuilder outPut = new StringBuilder();
-         outPut.append("Dealer: ").append(dealer.displayHand()).append("\n");
-         outPut.append("Player: ").append(player.displayHand());
-        System.out.println(outPut);
+        StringBuilder output = new StringBuilder();
+        output.append("Dealer: ").append(dealer.displayHand()).append("\n");
+        output.append("Player: ").append(player.displayHand());
+        System.out.println(output);
+    }
 
+    private void deal() {
+        for (int count = 0; count < 2; count++) {
+            // list of hands
+            dealer.addCard(deck.draw());
+            player.addCard(deck.draw());
+        }
     }
 
     private void determineWinner() {
-         if (player.getValue() > dealer.getValue()) {
-             System.out.println("Player Wins");
-             return;
-         }
-
-         if (player.getValue() == dealer.getValue()) {
-             System.out.println("Push");
-             return;
-         }
+        if (player.getValue() > dealer.getValue()) {
+            System.out.println("Player Wins");
+            return;
+        }
+        if (player.getValue() == dealer.getValue()) {
+            System.out.println("Push");
+            return;
+        }
         System.out.println("Dealer Wins");
     }
 
@@ -55,32 +58,36 @@ public class Table {
         System.out.println("Dealer: " + dealer.displayHand());
         int action = activeHand.getAction();
         switch (action) {
-            case 0 -> System.out.println("Quit");
-            case 1 -> System.out.println("Hit");
-            case 2 -> System.out.println("Stand");
+            case 0 -> System.out.println("Quiting");
+            case 1 -> {
+                System.out.println("Hit me");
+                hit(activeHand);
+            }
+            case 2 -> System.out.println("Waves Hand");
             case 3 -> System.out.println("Double Down");
-            case 4 -> System.out.println("Split");
-            default -> System.out.println("ERROR bad action");
+            case 4 -> System.out.println("two hands");
+            default -> System.out.println("ERROR bad action " + action);
         }
     }
 
     private void hit(Hand activeHand) {
-        // TODO: 9/27/2021 hit
+        // TODO: hit
+        activeHand.addCard(deck.draw());
+        System.out.println(player.displayHand() +"\nHand total: " + player.getValue());
+
     }
 
     private void stand(Hand activeHand) {
-        // TODO: 9/27/2021 stand
+        // TODO: stand
+        System.out.println("end turn");
+
     }
 
     private void doubleDown(Hand activeHand) {
-        // TODO: 9/27/2021 doubleDown
+        // TODO: double
     }
 
     private void split(Hand activeHand) {
         doubleDown(activeHand);
     }
-
-
-
-
 }
